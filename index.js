6aaeb3cff021cec2f1855f08a3be064d43835110
MonsterBasket <blazza1@hotmail.com>
2022-07-21 10:50:08 +1000

    man, html specific javascript is wordy as f***

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.getElementById("main").remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.getElementsByTagName('body')[0].appendChild(newHeader);
+
+//probably not always possible, but it makes more sense to me to do it all in one longer line of code, rather than 4 short lines.
+document.getElementsByTagName('body')[0].innerHTML += ('<h1 id="biggerVictory">MY-NAME is the ULTIMATE champion</h1>');
